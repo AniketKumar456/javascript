dc679c5d1c38a233d3a76f861cb19fadde5545be
Aniket Kumar <aniketkumar290604@gmail.com>
2025-07-25 15:17:09 +0000

    datatypes.js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+let name = "Aniket"
+let age = 21
+let ispass = false
+let state
+
+console.log(name)
+console.log(age)
+console.log(ispass)
+console.log(state)
+
+console.table([typeof name, typeof age, typeof ispass, typeof state, typeof null]);
+
+
+
