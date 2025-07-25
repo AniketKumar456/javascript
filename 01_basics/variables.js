e11f4f92a721fe6ed41b3ac7146ed481b5ffc9da
Aniket Kumar <aniketkumar290604@gmail.com>
2025-07-25 10:28:08 +0000

    Variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+console.log("This is all about Variables")
+const accountId = 12345
+let accountemail = "aniketkumar290604@gmail.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountId);
